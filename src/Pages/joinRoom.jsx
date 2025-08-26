ed79c017de2ecd2e48ac57891524e5da4bb282f8
Hariprasad <hariprasad.vijay2001@gmail.com>
2025-08-26 20:35:15 +0530

    createAndJoinRoomChanges (#2)

---
+++ src/Pages/joinRoom.jsx
@@ -0,0 +1,47 @@
+import { useState } from "react";
+import { TextField, Button, Box, Autocomplete } from "@mui/material";
+
+function JoinRoom({ handleJoinRoom, options }) {
+  const [roomCode, setRoomCode] = useState("");
+  const [name, setName] = useState("")
+  const [team, setTeam] = useState("");
+
+
+  return (
+    <Box display="flex" flexDirection="column" gap={2} width="300px" m="auto">
+      <TextField
+        label="Enter Room Code"
+        variant="outlined"
+        value={roomCode}
+        onChange={(e) => setRoomCode(e.target.value)}
+        fullWidth
+      />
+       <TextField
+        label="Enter Your Name"
+        variant="outlined"
+        value={name}
+        onChange={(e) => setName(e.target.value)}
+        fullWidth
+      />
+      <Autocomplete
+          options={options}
+          value={team}
+          onChange={(event, newValue) => setTeam(newValue)}
+          fullWidth
+          renderInput={(params) => (
+           <TextField {...params} label="Select your Team" variant="outlined" />
+          )}
+      />
+      <Button
+        variant="contained"
+        color="secondary"
+        onClick={()=>handleJoinRoom(roomCode,name,team)}
+        fullWidth
+      >
+        Join Room
+      </Button>
+    </Box>
+  );
+}
+
+export default JoinRoom;
